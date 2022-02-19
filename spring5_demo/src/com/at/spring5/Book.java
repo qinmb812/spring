8dc026e6f5363d78e21852ddb28175e547128e0d
qinmb812 <2449338367@qq.com>
2022-02-19 10:56:41 +0800

    字面量

--- spring5_demo/src/com/at/spring5/Book.java
+++ spring5_demo/src/com/at/spring5/Book.java
@@ -7,6 +7,7 @@ public class Book {
     //创建属性
     private String bname;
     private String bauthor;
+    private String address;
 
     //创建属性对应的set方法
     public void setBname(String bname) {
@@ -17,7 +18,11 @@ public class Book {
         this.bauthor = bauthor;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void testDemo() {
-        System.out.println(bname + "::" + bauthor);
+        System.out.println(bname + "::" + bauthor + "::" + address);
     }
 }
