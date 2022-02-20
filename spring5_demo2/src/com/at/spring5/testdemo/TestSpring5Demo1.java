4bb0fb4271da33305748c1ff22b07098bf720632
qinmb812 <2449338367@qq.com>
2022-02-20 14:56:02 +0800

    把集合注入部分提取出来

--- spring5_demo2/src/com/at/spring5/testdemo/TestSpring5Demo1.java
+++ spring5_demo2/src/com/at/spring5/testdemo/TestSpring5Demo1.java
@@ -1,5 +1,6 @@
 package com.at.spring5.testdemo;
 
+import com.at.spring5.collectiontype.Book;
 import com.at.spring5.collectiontype.Student;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -7,9 +8,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring5Demo1 {
     @Test
-    public void testCollection() {
+    public void testCollection1() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         Student student = context.getBean("student", Student.class);
         student.test();
+    }
+
+    @Test
+    public void testCollection2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        Book book = context.getBean("book", Book.class);
+        book.test();
     }
 }
