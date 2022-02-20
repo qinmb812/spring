740dde6268bbd0022333bc51cff2a076ee45ddad
qinmb812 <2449338367@qq.com>
2022-02-20 20:55:30 +0800

    Spring注解--@Resource

--- spring5_demo3/src/com/at/spring5/service/UserService.java
+++ spring5_demo3/src/com/at/spring5/service/UserService.java
@@ -6,14 +6,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 // 在注解里面value属性值可以不写
 // 默认值是类名称，首字母小写
 // UserService -- userService
 // @Component(value = "userService")   // <bean id="userService" class=".."/>
 @Service
 public class UserService {
-    @Autowired    // 根据类型进行注入
-    @Qualifier("userDaoImpl")   // 根据名称进行注入
+//    @Autowired    // 根据类型进行注入
+//    @Qualifier("userDaoImpl")   // 根据名称进行注入
+//    private UserDao userDao;
+
+    //    @Resource   // 根据类型进行注入
+    @Resource(name = "userDaoImpl")
     private UserDao userDao;
 
     public void add() {
