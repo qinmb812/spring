b7e4433cfc4f680c35c7cb056ada0583f439960c
qinmb812 <2449338367@qq.com>
2022-02-20 21:13:40 +0800

    完全注解开发

--- spring5_demo3/src/com/at/spring5/testdemo/TestSpring5Demo1.java
+++ spring5_demo3/src/com/at/spring5/testdemo/TestSpring5Demo1.java
@@ -1,14 +1,25 @@
 package com.at.spring5.testdemo;
 
+import com.at.spring5.config.SpringConfig;
 import com.at.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring5Demo1 {
     @Test
-    public void testService(){
-        ApplicationContext context=new ClassPathXmlApplicationContext("bean1.xml");
+    public void testService1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        System.out.println(userService);
+        userService.add();
+    }
+
+    @Test
+    public void testService2() {
+        // 加载配置类
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         System.out.println(userService);
         userService.add();
