0dab47226fc91eafbeef3557bca222723499612f
qinmb812 <2449338367@qq.com>
2022-02-20 11:45:39 +0800

    内部bean和级联赋值

---
+++ spring5_demo/src/com/at/spring5/bean/Emp.java
@@ -0,0 +1,27 @@
+package com.at.spring5.bean;
+
+/**
+ * 员工类
+ */
+public class Emp {
+    private String ename;
+    private String gender;
+    // 员工属于某一个部门，使用对象形式表示
+    private Dept dept;
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void add() {
+        System.out.println(ename + "::" + gender + "::" + dept);
+    }
+}
--- spring5_demo/src/com/at/spring5/testdemo/TestBean.java
+++ spring5_demo/src/com/at/spring5/testdemo/TestBean.java
@@ -1,5 +1,6 @@
 package com.at.spring5.testdemo;
 
+import com.at.spring5.bean.Emp;
 import com.at.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -7,9 +8,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestBean {
     @Test
-    public void testAdd() {
-        ApplicationContext context=new ClassPathXmlApplicationContext("bean2.xml");
+    public void testBean1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
         UserService userService = context.getBean("userService", UserService.class);
         userService.add();
+    }
+
+    @Test
+    public void testBean2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean3.xml");
+        Emp emp = context.getBean("emp", Emp.class);
+        emp.add();
     }
 }
