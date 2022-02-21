6e712be7733f0fbc054720514c7c6e7b24e2d9ef
qinmb812 <2449338367@qq.com>
2022-02-21 13:33:23 +0800

    aop--AspectJ配置文件

---
+++ spring5_demo4/src/com/at/spring5/aopxml/Book.java
@@ -0,0 +1,7 @@
+package com.at.spring5.aopxml;
+
+public class Book {
+    public void buy() {
+        System.out.println("buy ...........");
+    }
+}
--- spring5_demo4/src/com/at/spring5/testdemo/TestAop.java
+++ spring5_demo4/src/com/at/spring5/testdemo/TestAop.java
@@ -1,6 +1,7 @@
 package com.at.spring5.testdemo;
 
 import com.at.spring5.aopanno.User;
+import com.at.spring5.aopxml.Book;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,5 +12,12 @@ public class TestAop {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         User user = context.getBean("user", User.class);
         user.add();
+    }
+
+    @Test
+    public void testAopXml() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        Book book = context.getBean("book", Book.class);
+        book.buy();
     }
 }
