2717613e8edca89d98c551290629ebadb0464f05
qinmb812 <2449338367@qq.com>
2022-02-23 21:36:47 +0800

    Webflux--函数式编程模型

--- webfluxdemo2/src/main/java/com/at/webfluxdemo1/Client.java
+++ webfluxdemo2/src/main/java/com/at/webfluxdemo1/Client.java
@@ -7,7 +7,7 @@ import reactor.core.publisher.Flux;
 
 public class Client {
     public static void main(String[] args) {
-        WebClient client = WebClient.create("http://127.0.0.1:52120");
+        WebClient client = WebClient.create("http://127.0.0.1:52395");
         int id = 1;
         User user = client.get().uri("/users/{id}", id).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(User.class).block();
         System.out.println(user.getName());
